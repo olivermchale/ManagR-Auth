10f416dbe50121f672a1f717cb6bd1d0853c9398
olivermchaleTU <olivermchale98@gmail.com>
2020-02-06 12:31:02 +0000

    Initial Commit

---
+++ UserAuthentication/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UserAuthentication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult SomeThing()
+        {
+            return Ok();
+        }
+
+        [Authorize]
+        public IActionResult GetSecret()
+        {
+            return Ok();
+        }
+
+        public IActionResult Login()
+        {
+            return Ok();
+        }
+    }
+}
