a0fac8cd68b3c09c38e077a76858e1b6c9f276b9
olivermchaleTU <olivermchale98@gmail.com>
2020-04-16 11:11:01 +0100

    Always migrate

--- UserAuthentication/Program.cs
+++ UserAuthentication/Program.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using UserAuthentication.Data;
 
 namespace UserAuthentication
 {
@@ -13,7 +16,15 @@ namespace UserAuthentication
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var env = services.GetRequiredService<IWebHostEnvironment>();
+                var context = services.GetRequiredService<UserAuthenticationDb>();
+                context.Database.Migrate();
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
